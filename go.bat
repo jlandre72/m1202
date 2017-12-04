2a49c4d92c838ef5f1f28374757c2d4e2ceb3482
Jérôme LANDRE <j.landre@laposte.net>
2017-12-04 18:04:53 +0100

    maj6

--- go.bat
+++ go.bat
@@ -4,7 +4,11 @@ pandoc -t revealjs -i -s %1.md -o %1-revealjs.html --css=nightjl.css --mathml -V
 
 pandoc -t slidy -s %1.md -o %1-slidy.html --css=stylejl.css --mathml
 
-pandoc -t latex --pdf-engine=lualatex -s %1.md -o %1.pdf
+pandoc -t s5 -s %1.md -o %1-s5.html --css=stylejl.css --mathml
+
+pandoc -t dzslides -s %1.md -o %1-dzslides.html --css=stylejl.css --mathml
+
+pandoc --self-contained -t latex --pdf-engine=lualatex -s %1.md -o %1.pdf
 
 pandoc -t html5 -s %1.md -o %1.html --css=nightjl.css --mathml
 
