d7de7114b7073e1718438619ad8ebe93a508957a
Jérôme LANDRE <j.landre@laposte.net>
2017-12-04 17:31:05 +0100

    maj4

--- cm.md
+++ cm.md
@@ -1,5 +1,15 @@
 # M1202 - Algorithmique et Programmation
 
+## Introduction à la programmation graphique : le langage LOGO
+
+### Page LOGO :
+
+Afin de découvrir comment fonctionne la programmation, on va étudier un langage simple de programmation graphique :
+
+- Le site se trouve ici : http://www.calormen.com/jslogo/
+
+
+
 ## Variables
 
 ## Types de données
--- go.bat
+++ go.bat
@@ -1,10 +1,10 @@
-pandoc -t revealjs -s m1202-cm1.txt -o m1202-cm1-revealjs.html --css=nightjl.css --mathml -V slideNumber=true
+pandoc -t revealjs -s %1.md -o %1-revealjs.html --css=nightjl.css --mathml -V slideNumber=true
 
-pandoc -t slidy -s m1202-cm1.txt -o m1202-cm1-slidy.html --css=stylejl.css --mathml
+pandoc -t slidy -s %1.md -o %1-slidy.html --css=stylejl.css --mathml
 
-pandoc -t pdf -s m1202-cm1.txt -o m1202-cm1-slidy.pdf
+pandoc -t pdf -s %1.md -o %1.pdf
 
-pandoc -t html5 -s php_m1202.md -o php_m1202.html --css=nightjl.css --mathml
+pandoc -t html5 -s %1.md -o %1.html --css=nightjl.css --mathml
 
 REM pause
 
